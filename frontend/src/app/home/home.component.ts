5a13b84563e0adeda358f3fff17120423f9eb1cd
JalilBhat <bhatjalilahmad@gmail.com>
2026-06-05 11:54:30 +0530

    Used switchMap

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { CommonModule } from '@angular/common';
+import { catchError } from 'rxjs/internal/operators/catchError';
+import { switchMap } from 'rxjs/internal/operators/switchMap';
 
 @Component({
   selector: 'app-home',
@@ -17,9 +19,19 @@ export class HomeComponent {
   ) {}
 
   logout() {
-    this.auth.logout().subscribe({
-      next: () => this.router.navigate(['/login']),
-      error: () => this.router.navigate(['/login']),
-    });
+    this.auth
+      .logout()
+      .pipe(
+        switchMap(() => this.router.navigate(['/login'])),
+        catchError(() => this.router.navigate(['/login'])),
+      )
+      .subscribe();
   }
+
+  // logout() {
+  //   this.auth.logout().subscribe({
+  //     next: () => this.router.navigate(['/login']),
+  //     error: () => this.router.navigate(['/login']),
+  //   });
+  // }
 }
